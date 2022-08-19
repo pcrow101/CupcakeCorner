f5243c8bdf987595a28ce1632f4c798e710cab52
pcrow101 <34235799+pcrow101@users.noreply.github.com>
2022-08-19 13:45:48 +0100

    wip

---
+++ CupcakeCorner/AsyncImageExample.swift
@@ -0,0 +1,29 @@
+//
+//  AsyncImageExample.swift
+//  CupcakeCorner
+//
+//  Created by paul crow on 14/08/2022.
+//
+//
+//import SwiftUI
+//
+//
+//struct ContentView: View {
+//    var body: some View {
+//        AsyncImage(url: URL(string: "https://hws.dev/img/logo2.png")) { image in
+//            image
+//                .resizable()
+//                .scaledToFit()
+//        } placeholder: {
+//            ProgressView()
+//        }
+//        .frame(width: 200, height: 200)
+//    }
+//        
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
---
+++ CupcakeCorner/ValidateAndDisableForms.swift
@@ -0,0 +1,36 @@
+//
+//  ValidateAndDisableForms.swift
+//  CupcakeCorner
+//
+//  Created by paul crow on 14/08/2022.
+//
+
+
+//import SwiftUI
+//
+//struct ContentView: View {
+//    @State private var username = ""
+//    @State private var email = ""
+//
+//    var body: some View {
+//        Form {
+//            Section {
+//                TextField("Username", text: $username)
+//                TextField("Email", text: $email)
+//            }
+//            
+//            Section {
+//                Button("Create account") {
+//                    print("Creating Account...")
+//                }
+//            }
+//            .disabled(username.isEmpty || email.isEmpty)
+//        }
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
